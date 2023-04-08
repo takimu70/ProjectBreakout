c4e4eddb973adf28c51fec90ebf946583efb2ca9
Oguz Tanil <oua_takim_u70@protonmail.com>
2023-04-08 17:21:07 +0300

    Base Files

---
+++ ProjectBreakout/Assets/Scripts/boxTrigger_Script.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxTrigger_Script : MonoBehaviour
+{
+    public GameObject computerScreenUI;
+
+    private bool isInRange = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInRange = false;
+            computerScreenUI.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            computerScreenUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else if (isInRange && Input.GetKeyDown(KeyCode.Escape))
+        {
+            computerScreenUI.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
